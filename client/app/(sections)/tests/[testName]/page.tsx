e069e146fa1b3d960d140d2ef41b97296bcd3d05
Ahmed Osama <ao6155518@gmail.com>
2024-09-29 00:27:49 +0300

    for successful deployment

--- client/app/(sections)/tests/[testName]/page.tsx
+++ client/app/(sections)/tests/[testName]/page.tsx
@@ -1,24 +1,25 @@
 "use client"; // أضف هذه السطر في أعلى الملف
 
-import { useParams } from 'next/navigation';
-import React from 'react';
+import { useParams } from "next/navigation";
+import React from "react";
 
 const TestPage: React.FC = () => {
-  const { testName } = useParams();
-
+  const params = useParams();
+  const testName = params?.testName as string;
   return (
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold text-gray-800 mb-6">
-          {testName ? testName.replace(/-/g, ' ') : 'Loading...'}
+          {testName ? testName.replace(/-/g, " ") : "Loading..."}
         </h1>
         <p className="text-lg text-gray-600">
-          This is the detailed page for the {testName ? testName.replace(/-/g, ' ') : 'test'}. 
-          Here you can start the test or see more information about it.
+          This is the detailed page for the{" "}
+          {testName ? testName.replace(/-/g, " ") : "test"}. Here you can start
+          the test or see more information about it.
         </p>
         {/* Add more content for the test page here */}
       </div>
-    </div>
+    </div> // Use type assertion to treat params as Params
   );
 };
 
