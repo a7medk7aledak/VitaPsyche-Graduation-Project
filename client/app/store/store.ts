0983a7fdbc31ebf87b69569d5fa6832121d9f607
Belal Ahmed <belal.ahmed121sq1@gmail.com>
2024-12-30 23:24:30 +0200

    fix deplyment

--- client/app/store/store.ts
+++ client/app/store/store.ts
@@ -1,15 +1,22 @@
+// store/store.ts
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import authReducer from "./authSlice";
 import chatReducer from "./chatSlice";
-import { useDispatch } from "react-redux";
+import doctorFormReducer from "./authDoctor/authDoctorSlice";
+import cartReducer from "./cartSlice"; // تأكد من صحة مسار الاستيراد
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     chat: chatReducer,
+    doctorForm: doctorFormReducer,
+    cart: cartReducer, // إضافة cart reducer
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
