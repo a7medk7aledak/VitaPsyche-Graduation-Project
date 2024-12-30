2a3a4113cf4d00050f1493e7b09fdeecb6ac517d
Belal Ahmed <belal.ahmed121sq1@gmail.com>
2024-12-30 22:56:53 +0200

    Update store.ts

--- client/app/store/store.ts
+++ client/app/store/store.ts
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
 import chatReducer from "./chatSlice";
+import doctorFormReducer from "./authDoctor/authDoctorSlice";
+import { useDispatch } from "react-redux";
 
 export const store = configureStore({
   reducer: {
@@ -11,3 +13,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
